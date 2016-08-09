523e8eae510f32cc2edef47da676a6d63769b7d9
Luis Lezcano Airaldi <luislezcair@gmail.com>
2016-08-09 19:11:42 -0300

    Fixed authentication bug.
    * Fixed a bug that caused authentication to fail when running on a
    production server for a few days. Bravo was not realoding the temp file with
    token and signature information.

--- lib/bravo/auth_data.rb
+++ lib/bravo/auth_data.rb
@@ -1,11 +1,8 @@
 module Bravo
-
   # This class handles authorization data
   #
   class AuthData
-
     class << self
-
       attr_accessor :environment, :todays_data_file_name
 
       # Fetches WSAA Authorization Data to build the datafile for the day.
@@ -13,13 +10,15 @@ module Bravo
       # to be configured as Bravo.pkey and Bravo.cert
       #
       def fetch
-        raise "Archivo de llave privada no encontrado en #{ Bravo.pkey }" unless File.exist?(Bravo.pkey)
-        raise "Archivo certificado no encontrado en #{ Bravo.cert }" unless File.exist?(Bravo.cert)
+        raise "Archivo de llave privada no encontrado en #{Bravo.pkey}" unless File.exist?(Bravo.pkey)
+        raise "Archivo certificado no encontrado en #{Bravo.cert}" unless File.exist?(Bravo.cert)
 
         Bravo::Wsaa.login unless File.exist?(todays_data_file_name)
 
         YAML.load_file(todays_data_file_name).each do |k, v|
-          Bravo.const_set(k.to_s.upcase, v) unless Bravo.const_defined?(k.to_s.upcase)
+          const = k.to_s.upcase
+          Bravo.remove_const(const) if Bravo.const_defined?(const)
+          Bravo.const_set(const, v)
         end
       end
 
